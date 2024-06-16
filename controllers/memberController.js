83ecdfe115c1f5a7f2bb60e369e15e68cec714cb
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-06-16 16:51:05 +0100

    rework POST function handler for member creation, includes crypotgraphic method via bcryptjs

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -1,4 +1,5 @@
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
 const Member = require('../models/member');
 
 exports.member_create_get = asyncHandler(async (req, res, next) => {
@@ -12,11 +13,12 @@ exports.member_create_post = [
   // validation
 
   asyncHandler(async (req, res, next) => {
-    const member = new Member({
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    await Member.create({
       username: req.body.username,
-      password: req.body.password,
+      password: hashedPassword,
     });
-    await member.save();
   }),
 
 ];
