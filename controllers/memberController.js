8a6ea23a6631da378a0f977c4a801bfd578ff092
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-06-17 19:02:04 +0100

    add validation/santisation with error handling to 'member_create_post'

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -1,24 +1,50 @@
 const asyncHandler = require('express-async-handler');
 const bcrypt = require('bcryptjs');
+const { body, validationResult } = require('express-validator');
 const Member = require('../models/member');
 
 exports.member_create_get = asyncHandler(async (req, res, next) => {
-  res.render('sign_up_get', {
+  res.render('sign_up', {
     title: 'Sign-up Form',
   });
 });
 
 exports.member_create_post = [
 
-  // validation
+  body('username')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Please enter a username'),
+  body('password')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('password-confirm')
+    .escape()
+    .custom((value, { req }) => value === req.body.password)
+    .withMessage('Passwords must match'),
 
   asyncHandler(async (req, res, next) => {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const errors = validationResult(req);
 
-    await Member.create({
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const member = new Member({
       username: req.body.username,
       password: hashedPassword,
     });
+
+    if (!errors.isEmpty()) {
+      // there are errors
+      res.render('sign_up', {
+        title: 'Sign-up Form',
+        username: member.username,
+        errors: errors.array(),
+      });
+    } else {
+      await member.save();
+      res.redirect('/members/sign-in');
+    }
   }),
 
 ];
