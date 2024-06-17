a00350379d51dc61e1daaf7ff484da86b0d2399f
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-06-17 18:56:41 +0100

    add 'member' property to MembersSchema (membership status:boolean)

--- models/member.js
+++ models/member.js
@@ -5,6 +5,7 @@ const { Schema } = mongoose;
 const MembersSchema = new Schema({
   username: { type: String, required: true },
   password: { type: String, require: true },
+  member: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model('Members', MembersSchema);
