9fa9308a291a1343f9bb5825a5c0dbcd7e48dec1
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-06-18 20:01:03 +0100

    add username property to render method's locals object

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,16 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+
+  if (req.user) {
+    const test = req.user
+    console.log(test)
+  }
+
+  res.render('index', { 
+    user: req.user,
+    username: req.user.username,
+  });
 });
 
 module.exports = router;
