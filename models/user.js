d258d3fb67fe6670f30f648a5b779efd3af5f48b
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-06-21 22:56:53 +0100

    add 'admin' property to UserSchema (admin status: boolean)

--- models/user.js
+++ models/user.js
@@ -6,6 +6,7 @@ const UserSchema = new Schema({
   username: { type: String, required: true },
   password: { type: String, require: true },
   member: { type: Boolean, default: false },
+  admin: { type: Boolean, default: false }
 });
 
 module.exports = mongoose.model('Users', UserSchema);
