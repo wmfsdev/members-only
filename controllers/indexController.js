28a704c303b7275e5aa0c88ca00c1ca8ea0ee63e
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-06-24 18:30:25 +0100

    add admin property to render object and create function handler for submitting message delete request

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -4,28 +4,33 @@ const Message = require('../models/message');
 
 exports.index = asyncHandler( async(req, res, next) => {
 
-	const messages = await Message.find({}).populate("user", "username")
+	const messages = await Message.find({}).populate("user",  ["username", "admin"])
 	let name
-
-	if (req.user) name = req.user.username
-	else name = null
+	let admin
+	
+	if (req.user) name = req.user.username, admin = req.user.admin
+	else name = null, admin = null
 	
   	res.render('index', { 
 		user: req.user,
 		username: name,
+		admin: admin,
 		messages: messages,
   	});
 });
+
+exports.index_message_post = asyncHandler( async(req, res, next) => {
+	await Message.findByIdAndDelete(req.params.id)
+	res.redirect('/')
+})
   
 exports.index_post = [
 
 	body('title')
-		.trim()
 		.isLength({ min: 1 })
 		.escape()
 		.withMessage('Title too short'),
 	body('message')
-		.trim()
 		.isLength({ min: 5 })
 		.escape()
 		.withMessage("Message too short"),
@@ -42,5 +47,4 @@ exports.index_post = [
 		await message.save()
 		res.redirect('/')
 	})
-
 ]
